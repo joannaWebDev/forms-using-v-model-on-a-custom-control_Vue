6b9220a62be7526f899618483cc9bc440b8fdbff
Joanna M <joannasmerea@Glovos-MBP.home>
2021-07-19 08:37:29 +0200

    forms-08-using-v-model-on-a-custom-control

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
